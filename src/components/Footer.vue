64558cf3057f7f194e8992845f00a20fbb1822c2
Andrea Coronese <sixpounder@protonmail.com>
2025-07-16 01:59:39 +0200

    feat: change formal cv address

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,12 @@
 <template>
   <footer class="md:container md:mx-auto mx-8 mb-8 text-sm text-gray-300">
     <span>
-      Source code for this site is available at <a title="Site source code" href="https://github.com/sixpounder/showcase" target="noopener">this repository</a>
+      Source code for this site is available at <a title="Site source code"
+        href="https://github.com/sixpounder/showcase" target="noopener">this repository</a>
     </span>
     <span>
-      If you want a more formal version of this, you can find a resume <a title="Link to resume" href="https://frosty-tereshkova-be782e.netlify.app/" target="noopener">at this link</a>
+      If you want a more formal version of this, you can find a resume <a title="Link to resume"
+        href="https://andrea-coronese-cv.netlify.app/" target="noopener">at this link</a>
     </span>
   </footer>
 </template>
